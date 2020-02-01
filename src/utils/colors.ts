4a5f25e1c4bfdafbd2c6e144e7d91731989bf16c
Jacob Weber <jacob@jacobweber.com>
2020-02-01 09:59:57 -0800

    to ts

--- src/utils/colors.ts
+++ src/utils/colors.ts
@@ -1,20 +1,17 @@
-export function lightenColor(col, amt) {
-	col = col.slice(1);
+export function lightenColor(color: string, amt: number): string {
+	color = color.slice(1);
 
-	const num = parseInt(col,16);
+	const num = parseInt(color, 16);
 
 	let r = (num >> 16) + amt;
-
 	if (r > 255) r = 255;
 	else if  (r < 0) r = 0;
 
 	let b = ((num >> 8) & 0x00FF) + amt;
-
 	if (b > 255) b = 255;
 	else if  (b < 0) b = 0;
 
 	let g = (num & 0x0000FF) + amt;
-
 	if (g > 255) g = 255;
 	else if (g < 0) g = 0;
 
