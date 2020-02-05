9b03789489e37db19271f6a2e233e2941b0f245e
Jacob Weber <jacob@jacobweber.com>
2020-02-04 16:07:28 -0800

    replace semantic-ui-css with fomantic-ui-css; fixes alert positioning bug

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
-import 'semantic-ui-css/semantic.min.css';
+import 'fomantic-ui-css/semantic.min.css';
 import './index.css';
 import { MainStore, StoreContext } from './stores/MainStore';
 import { App } from './components/App';
