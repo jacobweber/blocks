df1e685885e8e010e7538c071020e2fb4e5f21bf
Jacob Weber <jacob@jacobweber.com>
2020-02-05 21:44:11 -0800

    rename fn

--- src/stores/NewGameStore.ts
+++ src/stores/NewGameStore.ts
@@ -1,33 +1,33 @@
 import { decorate, observable, action } from 'mobx';
 
-type CallbackType = (result: boolean, level: number, rows: number) => void;
+type DoneCallbackType = (result: boolean, level: number, rows: number) => void;
 
 class NewGameStore {
 	visible: boolean = false;
-	callback: CallbackType | null = null;;
+	doneCallback: DoneCallbackType | null = null;;
 	level: number = 1;
 	rows: number = 0;
 
-	dialogShow(level: number, rows: number, callback: CallbackType) {
+	dialogShow(level: number, rows: number, doneCallback: DoneCallbackType) {
 		this.visible = true;
 		this.level = level;
 		this.rows = rows;
-		this.callback = callback;
+		this.doneCallback = doneCallback;
 	}
 
 	dialogCancel() {
 		this.visible = false;
-		if (this.callback) {
-			this.callback(false, 0, 0);
-			delete this.callback;
+		if (this.doneCallback) {
+			this.doneCallback(false, 0, 0);
+			delete this.doneCallback;
 		}
 	}
 
 	dialogOK(level: number, rows: number) {
 		this.visible = false;
-		if (this.callback) {
-			this.callback(true, level, rows);
-			delete this.callback;
+		if (this.doneCallback) {
+			this.doneCallback(true, level, rows);
+			delete this.doneCallback;
 		}
 	}
 }
