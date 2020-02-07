581bfbcc3301812a929dc0c32136a0638943740d
Jacob Weber <jacob@jacobweber.com>
2020-02-06 19:59:56 -0800

    fix keys

--- src/components/points/Standard.tsx
+++ src/components/points/Standard.tsx
@@ -12,15 +12,17 @@ const PointDefs = () => {
 	return (
 		<svg>
 			<defs>
-				{[...blockDefs, ...extraDefs].map((type, index) => (<>
-					<linearGradient key={index} id={`g${index}`} x1='0%' y1='0%' x2='100%' y2='100%'>
-						<stop stopColor={lightenColor(type.color, -50)} offset='0%' />
-						<stop stopColor={type.color} offset='80%' />
-					</linearGradient>
-					<symbol key={index} id={type.id}>
-						<rect fill={`url(#g${index})`} width='100%' height='100%' stroke='black' strokeWidth='1' />
-					</symbol>
-				</>))}
+				{[...blockDefs, ...extraDefs].map((type, index) => (
+					<React.Fragment key={index}>
+						<linearGradient id={`g${index}`} x1='0%' y1='0%' x2='100%' y2='100%'>
+							<stop stopColor={lightenColor(type.color, -50)} offset='0%' />
+							<stop stopColor={type.color} offset='80%' />
+						</linearGradient>
+						<symbol id={type.id}>
+							<rect fill={`url(#g${index})`} width='100%' height='100%' stroke='black' strokeWidth='1' />
+						</symbol>
+					</React.Fragment>
+				))}
 			</defs>
 		</svg>
 	);
