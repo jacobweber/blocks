53693a65d12f9d98567874595d54d82c11708efa
Jacob Weber <jacob@jacobweber.com>
2020-02-09 14:06:19 -0800

    ignore sidebar content height

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -24,19 +24,21 @@ const App = observer(() => {
 					<Board />
 				</div>
 				<div className={styles.right}>
-					<div className={styles.nextBlock}>
-						<NextBlock />
-					</div>
-					<div className={styles.scoreBoard}>
-						<ScoreBoard />
-					</div>
-					<div className={styles.highScores}>
-						<Button onClick={e => mainStore.showHighScores()}>High Scores</Button>
-						{highScoresStore.visible && <HighScores />}
-					</div>
-					<div className={styles.preferences}>
-						<Button onClick={e => mainStore.showPrefs()}>Preferences</Button>
-						{preferencesStore.visible && <Preferences />}
+					<div className={styles.content}>
+						<div className={styles.nextBlock}>
+							<NextBlock />
+						</div>
+						<div className={styles.scoreBoard}>
+							<ScoreBoard />
+						</div>
+						<div className={styles.highScores}>
+							<Button onClick={e => mainStore.showHighScores()}>High Scores</Button>
+							{highScoresStore.visible && <HighScores />}
+						</div>
+						<div className={styles.preferences}>
+							<Button onClick={e => mainStore.showPrefs()}>Preferences</Button>
+							{preferencesStore.visible && <Preferences />}
+						</div>
 					</div>
 				</div>
 			</div>
