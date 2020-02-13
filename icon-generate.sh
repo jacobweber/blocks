1c9ecc32d8b0359364d6c75950861cdd77980aca
Jacob Weber <jacob@jacobweber.com>
2020-02-12 16:14:27 -0800

    use moved logo

--- icon-generate.sh
+++ icon-generate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # https://realfavicongenerator.net/ for favicon.ico
-svg2png icon.svg -w 192 -h 192 -o logo192.png
-svg2png icon.svg -w 512 -h 512 -o logo512.png
+svg2png src/assets/logo.svg -w 192 -h 192 -o logo192.png
+svg2png src/assets/logo.svg -w 512 -h 512 -o logo512.png
