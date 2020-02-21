390d7eda7e0a187ede28753e024bff311e747c52
Jacob Weber <jacob@jacobweber.com>
2020-02-20 16:42:31 -0800

    styles

--- src/components/LayoutHorizTouch.tsx
+++ src/components/LayoutHorizTouch.tsx
@@ -19,22 +19,24 @@ const LayoutHorizTouch = observer(() => {
 			</div>
 			<div className={styles.right}>
 				<div className={styles.content}>
-					<div className={styles.buttonsTop}>
-						{(mainStore.gameState === GameState.Active || mainStore.gameState === GameState.Paused) ? (
-							<Button icon='stop' onClick={e => mainStore.endGame()} />
-						) : (
-							<Button icon='play' onClick={e => mainStore.newGame()} />
-						)}
-						{mainStore.gameState === GameState.Active && (
-							<Button icon='pause' onClick={e => mainStore.pause()} />
-						)}
-						{mainStore.gameState === GameState.Paused && (
-							<Button icon='play' onClick={e => mainStore.resume()} />
-						)}
-					</div>
-					<div className={styles.buttonsTop}>
-						<Button icon='trophy' onClick={e => mainStore.showHighScores()} />
-						<Button icon='setting' onClick={e => mainStore.showPrefs()} />
+					<div className={styles.buttons}>
+						<div className={styles.buttonsCol}>
+							{(mainStore.gameState === GameState.Active || mainStore.gameState === GameState.Paused) ? (
+								<Button icon='stop' onClick={e => mainStore.endGame()} />
+							) : (
+								<Button icon='play' onClick={e => mainStore.newGame()} />
+							)}
+							{mainStore.gameState === GameState.Active && (
+								<Button icon='pause' onClick={e => mainStore.pause()} />
+							)}
+							{mainStore.gameState === GameState.Paused && (
+								<Button icon='play' onClick={e => mainStore.resume()} />
+							)}
+						</div>
+						<div className={styles.buttonsCol}>
+							<Button icon='trophy' onClick={e => mainStore.showHighScores()} />
+							<Button icon='setting' onClick={e => mainStore.showPrefs()} />
+						</div>
 					</div>
 					<NextBlock className={styles.nextBlock} />
 					<ScoreBoard className={styles.scoreBoard} />
