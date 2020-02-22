f6d5f9ce26f4aedc5d71ec8265bacc2af2d79e85
Jacob Weber <jacob@jacobweber.com>
2020-02-22 15:09:45 -0800

    add cordova

--- src/index.tsx
+++ src/index.tsx
@@ -8,20 +8,32 @@ import * as serviceWorker from './serviceWorker';
 import { MainStore, StoreContext } from 'stores/MainStore';
 import { App } from 'components/App';
 
-configure({ enforceActions: "observed" });
+const startApp = () => {
+	configure({ enforceActions: "observed" });
 
-const mainStore = new MainStore();
-mainStore.initWindowEvents();
-(window as any).mainStore = mainStore;
+	const mainStore = new MainStore();
+	mainStore.initWindowEvents();
+	(window as any).mainStore = mainStore;
 
-ReactDOM.render(
-	<StoreContext.Provider value={mainStore}>
-		<App />
-	</StoreContext.Provider>,
-	document.getElementById('root')
-);
+	ReactDOM.render(
+		<StoreContext.Provider value={mainStore}>
+			<App />
+		</StoreContext.Provider>,
+		document.getElementById('root')
+	);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+	if ((navigator as any).splashscreen) {
+		(navigator as any).splashscreen.hide();
+	}
+
+	// If you want your app to work offline and load faster, you can change
+	// unregister() to register() below. Note this comes with some pitfalls.
+	// Learn more about service workers: https://bit.ly/CRA-PWA
+	serviceWorker.unregister();
+};
+
+if ((window as any).cordova) {
+	document.addEventListener('deviceready', startApp, false);
+} else {
+	startApp();
+};
