edeb7acd6a0802a990579cb1c784b4e19a98c765
Jacob Weber <jacob@jacobweber.com>
2020-02-23 12:48:41 -0800

    use popup for color

--- src/components/preferences/ColorPicker.tsx
+++ src/components/preferences/ColorPicker.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { SketchPicker, ColorResult } from 'react-color'
 
 import styles from 'components/preferences/ColorPicker.module.css';
+import { Popup } from 'semantic-ui-react';
 
 type ColorPickerProps = {
 	value: string;
@@ -11,23 +12,23 @@ type ColorPickerProps = {
 const ColorPicker = ({ value, onChange }: ColorPickerProps) => {
 	const [ visible, setVisible ] = useState(false);
 
-	const handleClick = () => setVisible(!visible);
 	const handleClose = () => setVisible(false);
+	const handleOpen = () => setVisible(true);
 	const handleChange = (color: ColorResult) => onChange(color.hex);
 
 	const colorStyles = { background: value };
 
 	return (
 		<div className={styles.root}>
-			<div className={styles.swatch} onClick={handleClick}>
-				<div className={styles.color} style={colorStyles} />
-			</div>
-			{visible && (
-				<div className={styles.popover}>
-					<div className={styles.cover} onClick={handleClose} />
-					<SketchPicker color={value} disableAlpha onChange={handleChange} />
+			<Popup on='click' trigger={
+				<div className={styles.swatch}>
+					<div className={styles.color} style={colorStyles} />
 				</div>
-			)}
+			} hideOnScroll open={visible} onClose={handleClose} onOpen={handleOpen}>
+				<Popup.Content className={styles.content}>
+					<SketchPicker color={value} disableAlpha onChange={handleChange} />
+				</Popup.Content>
+			</Popup>
 		</div>
 	);
 };
